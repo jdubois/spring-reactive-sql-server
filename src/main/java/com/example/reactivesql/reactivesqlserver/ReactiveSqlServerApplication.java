3454b4cc28a7a9804af1f62bff5f2c06049e3ba2
Julien Dubois <julien.dubois@gmail.com>
2019-08-27 16:35:41 +0200

    initial project creation

---
+++ src/main/java/com/example/reactivesql/reactivesqlserver/ReactiveSqlServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivesql.reactivesqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSqlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivesql/reactivesqlserver/ReactiveSqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivesql.reactivesqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
