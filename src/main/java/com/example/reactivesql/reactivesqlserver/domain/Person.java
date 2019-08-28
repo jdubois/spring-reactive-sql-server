525d696e2ba252d90c301614d3d3e0e6f1b04ec4
Julien Dubois <julien.dubois@gmail.com>
2019-08-28 14:37:24 +0200

    Create domain object

--- src/main/java/com/example/reactivesql/reactivesqlserver/domain/Person.java
+++ src/main/java/com/example/reactivesql/reactivesqlserver/domain/Person.java
@@ -1,4 +1,52 @@
 package com.example.reactivesql.reactivesqlserver.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("person")
 public class Person {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
+
+
