f2fa616b76accd187356c21f9d809665c83236e3
Julien Dubois <julien.dubois@gmail.com>
2019-08-29 09:53:54 +0200

    Use correct repository API

--- src/main/java/com/example/reactivesql/reactivesqlserver/repository/PersonRepository.java
+++ src/main/java/com/example/reactivesql/reactivesqlserver/repository/PersonRepository.java
@@ -1,9 +1,9 @@
 package com.example.reactivesql.reactivesqlserver.repository;
 
 import com.example.reactivesql.reactivesqlserver.domain.Person;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends R2dbcRepository<Person, Long> {
+public interface PersonRepository extends ReactiveCrudRepository<Person, Long> {
 }
