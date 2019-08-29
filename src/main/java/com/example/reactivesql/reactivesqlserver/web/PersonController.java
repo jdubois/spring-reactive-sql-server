06306f94d104714ad2ed82b1bbd71189cb4b5bce
Julien Dubois <julien.dubois@gmail.com>
2019-08-29 09:54:12 +0200

    Code REST controller

--- src/main/java/com/example/reactivesql/reactivesqlserver/web/PersonController.java
+++ src/main/java/com/example/reactivesql/reactivesqlserver/web/PersonController.java
@@ -1,16 +1,24 @@
 package com.example.reactivesql.reactivesqlserver.web;
 
+import com.example.reactivesql.reactivesqlserver.domain.Person;
 import com.example.reactivesql.reactivesqlserver.repository.PersonRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api")
 public class PersonController {
 
     private final PersonRepository personRepository;
 
     public PersonController(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    @GetMapping("/persons")
+    public Flux<Person> list() {
+        return personRepository.findAll();
     }
 }
